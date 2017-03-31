2560a4cca8d6230fdb679f48242cdc0087ea5af5
= <alexander.alegre@outlook.com>
2017-03-31 10:19:23 -0700

    playing with express.js

---
+++ node-web-server/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+// make app
+var app = express();
+
+// maintenance page
+app.use( (req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use( (req, res, next) => {
+    var now = new Date().toString();    
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append file');
+        }
+    });
+    next();
+});
+
+
+
+
+hbs.registerHelper('currentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my site!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error: 'Unable to fulfill this request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is on http://localhost:3000/');
+});
