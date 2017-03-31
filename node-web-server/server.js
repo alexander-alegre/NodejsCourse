e20c2680e055aa8a15e262e94d62fff535d13c6d
= <alexander.alegre@outlook.com>
2017-03-31 13:35:15 -0700

    added heroku connection

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 
 // make app
 var app = express();
@@ -54,6 +55,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is on http://localhost:3000/');
+app.listen(port, () => {
+    console.log(`Server is on port ${port}`);
 });
